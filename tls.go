fa1ed62317ae62bf424dadaa61b1c549719e7349
hslam <791874158@qq.com>
2020-09-23 11:20:48 +0800

    update TLS

--- tls.go
+++ tls.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 )
 
+// LoadTLSConfig returns a TLS config by loading the certificate file and the key file.
 func LoadTLSConfig(certFile, keyFile string) (*tls.Config, error) {
 	certPEMBlock, err := ioutil.ReadFile(certFile)
 	if err != nil {
@@ -20,6 +21,7 @@ func LoadTLSConfig(certFile, keyFile string) (*tls.Config, error) {
 	return TLSConfig(certPEMBlock, keyPEMBlock), nil
 }
 
+// TLSConfig returns a TLS config by the certificate data and the key data.
 func TLSConfig(certPEM []byte, keyPEM []byte) *tls.Config {
 	tlsCert, err := tls.X509KeyPair(certPEM, keyPEM)
 	if err != nil {
@@ -28,62 +30,67 @@ func TLSConfig(certPEM []byte, keyPEM []byte) *tls.Config {
 	return &tls.Config{Certificates: []tls.Certificate{tlsCert}}
 }
 
+// DefalutTLSConfig returns a default TLS config.
 func DefalutTLSConfig() *tls.Config {
 	return TLSConfig(DefaultCertPEM, DefaultKeyPEM)
 }
+
+// SkipVerifyTLSConfig returns a insecure skip verify TLS config.
 func SkipVerifyTLSConfig() *tls.Config {
 	return &tls.Config{InsecureSkipVerify: true}
 }
 
+// DefaultKeyPEM represents the default private key data.
 var DefaultKeyPEM = []byte(`-----BEGIN PRIVATE KEY-----
-MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDQx2lxR73msUrD
-EthvjZCG8nmxY/m6ZXBRdaygc++Ie4baSHgS5DsLUcMEPhUOzzkUjjvc9dFy/ROD
-yw/hL1jmgC2a8bA+czWlK1V6tP/bkEzy9MZpUIM7aY48W0/t4O9ZaRML8zavfozo
-Vq5UXoS6meIzS5afg6vVNFB9bLV/d/AmT86BdZ0PYUzYg4KJcGT2nFJnEecjBFjj
-khIzhQs02IxaHH7xiddcIjZpzXEcaGFxJGecDFe+LFHIN9pgCG17kppK2Ebt93ck
-EpIM1LuiemX2CBckBlXCACrIVkeQ6pJTKcEMFbEcU9zYz8G9Apu3CgZApQUxuSlc
-VbdStupHAgMBAAECggEBAMfqm0I441Pa4u8Gxa+UDBfcacD+LaxZ5AJsvt9qBK1Z
-M5kjma7lUtCU+zu9wuZKcasIQ7RiwqvsQFqMAGmtr+AQTxs2YYB7S5wccZ6tYO67
-L6PZ1YAU84TZn7SV72SmZirknbNssim78WutTQNG+qvAHMTnlZSrPchLbuObS/SA
-hufs9fqxzBUeVXVE18vyhBq4i2SRyKcBxKEPRdzhbYem8SPi7hr/NnPomNUOr8Cw
-JGY0XsD6MVs4myzdpPRTNL8farzvpmLdJ9C1Nc8DKIwZZsoGkoMXXxtqJzYUYp7C
-deqUCMJY381R3mJUpUd6RU2ocL521CyA2YinGKljspkCgYEA60CC0B0I2cQ+W1Bf
-i/E5rItT91cseCJTnpH9m2ok3Pe4CiO/BLK9Ew9rjlHb7wNofch/EwNb4veIxRf8
-2MMjBuecTvqn/akirtPtjDPuQoKW7yBEKjqW8+yt97INHhMVkhMkoZ4JTmq2gWJg
-oT02Eq9YN52dpJjqYM7OvSywHPUCgYEA4zEyKo1/KWNNCCyBMlmPzRNyhxLk0h+t
-84UlwTKRogsQLgkZrseRMKakiHjF4Ez6qczC5MrHqH4I3e5RrAQP9uNQJIA0g+Ma
-mhpI5xq1ZnkCq/Z69K808trzFS4rAS7NZfwn8mV0ZXFHzmTDH1tDEvg+NXUXykFE
-zI15yYyXpMsCgYEA1QSaLvZLgFyplifGDMLGVY3n3yzJcJKsowZQ3PyVGp0Ywd2y
-Zv+uI2cwHjPTca7lXBhDsKS2/GLmLonVAzZXLjZlHELuAMu5QxNVo0GWuhTjtO3D
-q3VYINGsiYBpTlU7kATTg6DFjoMkdS3uj7IMl4i82cdX6qYofLZnD3c6lU0CgYBZ
-t1uwIiBNH8GTsL90Opnmyf84B+YEdC4lNDcsi+Omsee5xi42LujO5X+jxM2fPcbe
-ttVftBQUHXEy8qGd5BzJygoj39zdGBmxMSAI4ysvRCoh7juv1GB8ZqoHeyvQU8MY
-uvKrbhUA2jMY9gF3qHpcS1uFkK/MVunsPRIS3Uok8QKBgGq5Auz3ByjA9gKPk8qO
-p8S35+ldnAnIsMeojW+rXmUBrHaSp9ea9+obFHp1JKjI4dMoT92EKRAU5nnM3iqR
-q2/umYjD1XcbHSRdxz8H7hkZfQ4GSAwG/KNeGWL5FRSq3p6WeobLL4WdTcugS5cz
-cXoVckT5dS+Mj/SxSJ/oyvyu
+MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDa3lfVcQZg3Ra2
+DCeTPM9I8cv35Y+R4niXJ7c2U9TvGE3l8zfsLBXtdN4bSlmaimOnOmfx0aVJ8XwL
+qcIMspJmzG9UlGdlOfirMTYCybvwhEf9bZc9lmLv27C4++4IljNF9sSv/Lnbdl5V
+Nr+lY1xKRR3HPpwwuJj3jh3TznzAnb0QnIRTyGGVThyE6uUQAgx8/taGenJDkzb7
+pry4kRvz+GgjAvhi/KOgxho7G6PLfzXeS+iPyaMg5npd3B90XIzaaXr4/yffC5BU
+bynHhZLmWKJXSp7brjiZzpFV8np7wKYrtqXW4My2MtMASnvfXrCfTwQ3FU0biBsk
+7dQHCcuzAgMBAAECggEAdN6zQhsHT+Pew7j7zOh0uzu6MZYYMssen4Aqmczr7/wn
+ZHmaS/dCgjicfTAXZqktC1fptzu+KhzToxqzrroP6OqTLDPOfkQVX7x4XcbBH25T
+TqUdVFqgW/oQhMap1VX27Q4W+u5VhDXRq2j/rt2+oz4C56isGGwJ6m6tyLMC9IqJ
+Ul9fHrLKKjHltYkCMYzbUP/9QVs9yMlw04BbxCvML21s3ikNuGc8qdQhoHkmxXns
+zUR9+P7CkMhSvhojs7MVgaflGozNna89MYAgX+0mCGkWqOXEoFN3n4HdxwW1nBHC
+34YndQdOsViO7j9o1SJMOBLMXiQexH+YDJMvjZpsEQKBgQDynyxfINLnHUz1Wo8K
+Z1dwmP+dd2av/MbBVsEEyxAugLW8a7Ks6bDlxk8VKB4GAkqzx6Ap+YywZDRJewKn
+XUoEG8TPo4dZBy3ttyXTk240zDi/NIJtVRhGxeOGX8zcmwtGHjq694RYeCDrMDWp
+yRCJHVUSYUhHwtVwvSZK8JSKCwKBgQDm798FKiqh8UFyuOwKkfdzPYjM6fDg3JuR
+E7kmyaeFRz1X0c9zZcWE+ehf9nZnwfU04ZL1WIrjkYWUBxcrBFjChAmFKqFOf97m
+0w8jCifuBu+AzaSfW39rzcbpCof9GIHTEczGIjIbj62NQfxhKejP/eA///o81Cf2
+hSnUpjn1+QKBgE0jyLLSN9wdl8tmqJYRN17odlU1kmOgBf2QvLvuaE2wxJeM0nlh
+r8nOnHRIlgspDWFNtiHCYzXuFiXKw5Q89/yIa7Hs92qZ+sNa+N7lQCPvTpeUdWeX
+p6lQ379olDUL4rC/icLKUbzjLOw6HsXF1MkTl2nJnnaafsxih1tKVJ/zAoGAEed8
++fCH96A1u8g8fKFOdv/JUGG+zCAua3QFAc3WkA2y4tEgbUjxpFqfunjoOykdcqke
+dKkVs4j/uzdFg49Ftmb4OfvRH73oMSsh3EyYResBvJG09qnoWhpNFpo7atLwlcWm
+g6H5Eov0H6SDBaFzLFT5gty8sOSd6I3wbU0p5zkCgYAwQe8+M7Su2v3mA0vbxbGb
+W96El5n15YRa6JHOigC+5mBhXilnDE8qomFkfELDOnQ+hdkgqbFd7P1/+K5raV+I
+aGh+dZd2MKnLevVoMexu40NQLVyJTOqumG05NNgmfg7VE8QUbXKfz+9pmfYFSZGS
+Wx4EqMDhdG9wlTsHGb1I/Q==
 -----END PRIVATE KEY-----
 `)
 
+// DefaultCertPEM represents the default certificate data.
 var DefaultCertPEM = []byte(`-----BEGIN CERTIFICATE-----
-MIIDbjCCAlYCCQCZymVmCboLuDANBgkqhkiG9w0BAQsFADB4MQswCQYDVQQGEwJD
-TjELMAkGA1UECAwCQkoxCzAJBgNVBAcMAkJKMQ4wDAYDVQQKDAVobHNhbTEOMAwG
-A1UECwwFaHNsYW0xDjAMBgNVBAMMBWhzbGFtMR8wHQYJKoZIhvcNAQkBFhA3OTE4
-NzQxNThAcXEuY29tMCAXDTE5MTEwODEzNDYzNloYDzIxMTkxMDE1MTM0NjM2WjB4
-MQswCQYDVQQGEwJDTjELMAkGA1UECAwCQkoxCzAJBgNVBAcMAkJKMQ4wDAYDVQQK
-DAVobHNhbTEOMAwGA1UECwwFaHNsYW0xDjAMBgNVBAMMBWhzbGFtMR8wHQYJKoZI
-hvcNAQkBFhA3OTE4NzQxNThAcXEuY29tMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8A
-MIIBCgKCAQEA0MdpcUe95rFKwxLYb42QhvJ5sWP5umVwUXWsoHPviHuG2kh4EuQ7
-C1HDBD4VDs85FI473PXRcv0Tg8sP4S9Y5oAtmvGwPnM1pStVerT/25BM8vTGaVCD
-O2mOPFtP7eDvWWkTC/M2r36M6FauVF6EupniM0uWn4Or1TRQfWy1f3fwJk/OgXWd
-D2FM2IOCiXBk9pxSZxHnIwRY45ISM4ULNNiMWhx+8YnXXCI2ac1xHGhhcSRnnAxX
-vixRyDfaYAhte5KaSthG7fd3JBKSDNS7onpl9ggXJAZVwgAqyFZHkOqSUynBDBWx
-HFPc2M/BvQKbtwoGQKUFMbkpXFW3UrbqRwIDAQABMA0GCSqGSIb3DQEBCwUAA4IB
-AQBMfGE+zL6XMc/CJK59rRJQdypFG2gPxzpHi4XXTUfTAAYhZZRMYfiefxII8s5V
-MG+n+c1wU/nubE3xj9dgq7aIC1L3EPyVkWu/s8lPNWKMOO1FchZghBHYsImD5uM6
-sD1euV5nOmPnirK/vrfBuemGLtOFEgDrCEk39bd8AWLgrdpeqfVpW4K6QkDh1V4u
-Qe2ZVXa6qRwJ8dAvo79JmW8txruJ6/5s4Af7Gogr/F2BHYlbLMgdjAYTOT0X7mOs
-ZtmM8OaZrcg7EDEFHsV3k56S6i4EUH2VDOCz+v2BAJmWqDwVmjI4kEKKbgjI9fwv
-st022MDOYYU7dKz1lAiLJr1G
+MIIDfDCCAmQCCQCAHkBfX03BnTANBgkqhkiG9w0BAQsFADB/MQswCQYDVQQGEwJD
+TjELMAkGA1UECAwCQkoxEDAOBgNVBAcMB0JlaWppbmcxDjAMBgNVBAoMBUhTTEFN
+MQwwCgYDVQQLDANSJkQxEjAQBgNVBAMMCWhzbGFtLmNvbTEfMB0GCSqGSIb3DQEJ
+ARYQNzkxODc0MTU4QHFxLmNvbTAgFw0yMDA5MjMwMzE3NTdaGA8yMTIwMDgzMDAz
+MTc1N1owfzELMAkGA1UEBhMCQ04xCzAJBgNVBAgMAkJKMRAwDgYDVQQHDAdCZWlq
+aW5nMQ4wDAYDVQQKDAVIU0xBTTEMMAoGA1UECwwDUiZEMRIwEAYDVQQDDAloc2xh
+bS5jb20xHzAdBgkqhkiG9w0BCQEWEDc5MTg3NDE1OEBxcS5jb20wggEiMA0GCSqG
+SIb3DQEBAQUAA4IBDwAwggEKAoIBAQDa3lfVcQZg3Ra2DCeTPM9I8cv35Y+R4niX
+J7c2U9TvGE3l8zfsLBXtdN4bSlmaimOnOmfx0aVJ8XwLqcIMspJmzG9UlGdlOfir
+MTYCybvwhEf9bZc9lmLv27C4++4IljNF9sSv/Lnbdl5VNr+lY1xKRR3HPpwwuJj3
+jh3TznzAnb0QnIRTyGGVThyE6uUQAgx8/taGenJDkzb7pry4kRvz+GgjAvhi/KOg
+xho7G6PLfzXeS+iPyaMg5npd3B90XIzaaXr4/yffC5BUbynHhZLmWKJXSp7brjiZ
+zpFV8np7wKYrtqXW4My2MtMASnvfXrCfTwQ3FU0biBsk7dQHCcuzAgMBAAEwDQYJ
+KoZIhvcNAQELBQADggEBAA4rrtWczvjVpttxJ7pbXQlmvVrakPwqqKEQ09hxcoqY
+EKkCucjJwFFQi1fNQBKpb+3BwlHIcfqdwpURiTwQjPmRgVhqdFqHE5pNF9EXdNm7
+zaylUiu+ySKKHHnCVagM7UszovCoRYY3hq75UsGwR+9WWxOoWRz43NdOTBBDE9y7
+JkRowySk9JE5isec+G0tDf6Fyj/3zWshWQalEH/Aq1Af0BMtWQL4VYXbealqK6rq
+MOwPd7m67gCJlNREX2JnMDBM2A9QcAIzhYrHBx5w6UhUwSL6IFhJzdFXl4klsKUQ
+cmw7rbPxsuPIyPlCobdtFoVpFN5vnOnF42nCb8tr0Xs=
 -----END CERTIFICATE-----
 `)
